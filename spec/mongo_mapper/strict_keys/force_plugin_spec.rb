8aeaab7a07bed1ea6a54b8e1bc6122d7b0a05ccc
Scott Gonyea <me@sgonyea.com>
2011-06-07 16:19:34 -0700

    better spec folder structure

--- spec/mongo_mapper/strict_keys/force_plugin_spec.rb
+++ spec/mongo_mapper/strict_keys/force_plugin_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../spec_helper", File.dirname(__FILE__))
+require File.expand_path("../../spec_helper", File.dirname(__FILE__))
 
 require 'mongo_mapper/strict_keys/force_plugin'
 
