3061e83604e10f00d36525c4d83c898929b647fe
Scott Gonyea <me@sgonyea.com>
2011-06-10 10:01:36 -0700

    Added docs around ensure_key_exists. Added true as a return to possibly future-proof this method.

--- lib/mongo_mapper/strict_keys.rb
+++ lib/mongo_mapper/strict_keys.rb
@@ -11,10 +11,15 @@ module MongoMapper
       module ClassMethods; end
 
       module InstanceMethods
+        # @param [Symbol, String] name The name of the key to be ensured
+        # @return [true] This method either returns true or raises an exception
+        # @raise [ArgumentError] If 'name' has not been specified as a key
+        # @see MongoMapper::Plugins::Keys#key
         def ensure_key_exists(name)
           unless respond_to?("#{name}=")
             raise ArgumentError.new("Attribute '#{name}' has not been explicitly defined as a key.")
           end
+          true
         end
       end
 
