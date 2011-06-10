5c3a767584bed641ec704b75ece9d67d8f8e7804
Scott Gonyea <me@sgonyea.com>
2011-06-10 10:10:42 -0700

    more sensible test (imo)

--- spec/mongo_mapper/strict_keys/force_plugin_spec.rb
+++ spec/mongo_mapper/strict_keys/force_plugin_spec.rb
@@ -5,46 +5,12 @@ describe 'MongoMapper::Plugins::StrictKeys -- Forced Plugin' do
 
   class ForcedModel
     include MongoMapper::Document
-
-    key :string_key, String
   end
 
-  describe 'Instance attributes' do
-    before(:each) do
-      @fm = ForcedModel.new
+  context 'MongoMapper::Plugins::StrictKeys Mixin' do
+    it 'should be present in the ancestors' do
+      ForcedModel.ancestors.should include(MongoMapper::Plugins::StrictKeys::InstanceMethods)
     end
-
-    context 'attributes-as-methods' do
-      it 'should accept attributes-as-methods for pre-defined keys' do
-        lambda {
-          @fm.string_key = "I work!"
-        }.should_not raise_error
-
-        @fm.string_key.should == "I work!"
-      end
-
-      it 'should reject attributes-as-methods for never-defined keys' do
-        lambda {
-          @fm.not_defined = 'blowed up'
-        }.should raise_error(NoMethodError)
-      end
-    end
-
-    context 'attributes-in-brackets' do
-      it 'should accept attributes-in-brackets for pre-defined keys' do
-        lambda {
-          @fm[:string_key] = "I work!"
-        }.should_not raise_error
-
-        @fm[:string_key].should == "I work!"
-      end
-
-      it 'should reject attributes-in-brackets for never-defined keys' do
-        lambda {
-          @fm[:not_defined] = 'blowed up'
-        }.should raise_error(ArgumentError)
-      end
-    end
-
   end
+
 end
